2477b9de8ab651a0d18e896313a388edd7088e59
Intrstng <intrstg@gmail.com>
2026-07-07 00:10:49 +0300

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,23 +7,22 @@ import { dependencies, devDependencies } from './package.json' // add to tsconfi
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(import.meta.dirname, 'src/index.ts'),
-      name: 'My-library-name',
-      // the proper extensions will be added
-      formats: ['es'],
+      entry: resolve(__dirname, 'src/index.ts'),
       fileName: 'index',
+      formats: ['es'],
+      name: 'live-bundlers',
     },
     rollupOptions: { // not rolldownOptions ???
       // make sure to externalize deps that shouldn't be bundled
       // into your library
       external: ['react/jsx-runtime', ...Object.keys(dependencies), ...Object.keys(devDependencies)],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        // globals: {
-        //   vue: 'Vue',
-        // },
-      },
+      // output: {
+      //   // Provide global variables to use in the UMD build
+      //   // for externalized deps
+      //   // globals: {
+      //   //   vue: 'Vue',
+      //   // },
+      // },
     },
     sourcemap: true,
     target: 'esnext',
